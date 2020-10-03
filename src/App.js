f58546477511beae64f71ed48c0931d3e73d485d
MalvinL645 <alvinfadil11@gmail.com>
2020-10-03 14:38:23 +0700

    useState class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState } from 'react';
 import './App.css';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 
 function App() {
+  const [name, setName] = useState('');
   return (
-    <div className="App">
-      <h1>My First Heading</h1>
-      <p>My first paragraph.</p>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>Hello World</title>
+      </Helmet>
+      <div className="App">
+        <ChangeName name={name} />
+        <button
+          onClick={() => {
+            setName('hee');
+          }}
+        >
+          change name
+        </button>
+      </div>
+    </HelmetProvider>
   );
 }
+
+const ChangeName = (props) => {
+  const { name } = props;
+  return (
+    <div className="">
+      <h1>My name is {name}</h1>
+    </div>
+  );
+};
 
 export default App;
