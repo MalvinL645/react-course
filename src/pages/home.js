5d32b41d71259811cc02eb5402ab9c8eea30f13c
MalvinL645 <alvinfadil11@gmail.com>
2020-10-31 13:38:55 +0700

    home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
-  return <div> Welcome Home!</div>;
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('yoi');
+  }, [count]);
+  return (
+    <div>
+      <h1>Welcome Home</h1>
+      <button
+        type="button"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
 };
 
 export default Home;
